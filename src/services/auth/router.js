7436e5654117e305cff65756428d7da08bff5e78
Salih Gumus <sudanmerinosu@gmail.com>
2022-05-14 12:38:10 +0300

    .

--- src/services/auth/router.js
+++ src/services/auth/router.js
@@ -3,6 +3,6 @@ const router = require('express').Router();
 const AuthController = require('./controllers');
 
 router.route('/login').post(AuthController.login);
-router.route('/register').post(AuthController.register);
+// router.route('/register').post(AuthController.register);
 
 module.exports = router;
